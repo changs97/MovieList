5ccd69d18261d4db3c0ea13a9b329ed80ecd434e
Changhyung <pck4949@naver.com>
2022-02-11 01:17:23 +0900

    Create FilmsModelItem.kt

---
+++ MovieList/app/src/main/java/com/changs/movielist/data/FilmsModelItem.kt
@@ -0,0 +1,24 @@
+package com.changs.movielist.data
+
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+
+data class FilmsModelItem (
+    var id: String,
+    val description: String,
+    val director: String,
+    val image: String,
+    val locations: List<String>,
+    val movie_banner: String,
+    val original_title: String,
+    val original_title_romanised: String,
+    val people: List<String>,
+    val producer: String,
+    val release_date: String,
+    val rt_score: String,
+    val running_time: String,
+    val species: List<String>,
+    val title: String,
+    val url: String,
+    val vehicles: List<String>,
+)
