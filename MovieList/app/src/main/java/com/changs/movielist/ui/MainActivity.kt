7a683b4d6a35198d9057e62ec0b409617d12dcc6
Changhyung <pck4949@naver.com>
2022-02-10 18:21:37 +0900

    Update MainActivity.kt

--- MovieList/app/src/main/java/com/changs/movielist/ui/MainActivity.kt
+++ MovieList/app/src/main/java/com/changs/movielist/ui/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.changs.movielist.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.viewpager2.widget.ViewPager2
+import com.changs.movielist.R
 import com.changs.movielist.config.BaseActivity
 import com.changs.movielist.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate)  {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 페이저에 어댑터 연결
+        binding.viewPager.adapter = ViewPagerAdapter(this)
+
+        // 슬라이드하여 페이지가 변경되면 바텀네비게이션의 탭도 그 페이지로 활성화
+        binding.viewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bottomNavigationView.menu.getItem(position).isChecked = true
+                }
+            }
+        )
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu1 -> {
+                    // ViewPager의 현재 item에 첫 번째 화면을 대입
+                    binding.viewPager.currentItem = 0
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu2 -> {
+                    // ViewPager의 현재 item에 두 번째 화면을 대입
+                    binding.viewPager.currentItem = 1
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu3 -> {
+                    // ViewPager의 현재 item에 세 번째 화면을 대입
+                    binding.viewPager.currentItem = 2
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    return@setOnItemSelectedListener false
+                }
+            }
+        }
+
     }
+
+
 }
