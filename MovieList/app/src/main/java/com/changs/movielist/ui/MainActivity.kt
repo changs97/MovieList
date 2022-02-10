7af22537a2c2ae49e3ef36aeba302f865ec6e85b
Changhyung <pck4949@naver.com>
2022-02-10 17:38:54 +0900

    Create MainActivity.kt

---
+++ MovieList/app/src/main/java/com/changs/movielist/ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.changs.movielist.ui
+
+import android.os.Bundle
+import com.changs.movielist.config.BaseActivity
+import com.changs.movielist.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate)  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
