2b3171d803e816763ecbaa6d9d0301a2d3d7d9a4
Changhyung <pck4949@naver.com>
2022-02-11 23:24:03 +0900

    Update LeftFragment.kt

--- MovieList/app/src/main/java/com/changs/movielist/ui/LeftFragment.kt
+++ MovieList/app/src/main/java/com/changs/movielist/ui/LeftFragment.kt
@@ -1,13 +1,12 @@
 package com.changs.movielist.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.changs.movielist.R
 import com.changs.movielist.config.BaseFragment
 import com.changs.movielist.databinding.FragmentLeftBinding
+import java.util.ArrayList
 
 
 class LeftFragment :  BaseFragment<FragmentLeftBinding>(FragmentLeftBinding::bind, R.layout.fragment_left) {
@@ -21,7 +20,9 @@ class LeftFragment :  BaseFragment<FragmentLeftBinding>(FragmentLeftBinding::bin
 
     private fun leftRecyclerSetUp() {
         val scoreSortedFilmsList = SplashActivity.filmsList.sortedByDescending{ it.rt_score }
-        adapter = RecyclerViewAdapter(scoreSortedFilmsList)
+        val scoreSortedFilmsArrayList = ArrayList(scoreSortedFilmsList)
+
+        adapter = RecyclerViewAdapter(scoreSortedFilmsArrayList)
         binding.leftFragmentRecyclerView.layoutManager = LinearLayoutManager(context)
         binding.leftFragmentRecyclerView.adapter = adapter
     }
