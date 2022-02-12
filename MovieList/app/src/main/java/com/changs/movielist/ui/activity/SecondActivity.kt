31d4c4f9b30102786c39f4946ffed3c0a1b7fbeb
Changhyung <pck4949@naver.com>
2022-02-12 15:30:47 +0900

    Create SecondActivity.kt

---
+++ MovieList/app/src/main/java/com/changs/movielist/ui/activity/SecondActivity.kt
@@ -0,0 +1,28 @@
+package com.changs.movielist.ui.activity
+
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.changs.movielist.config.BaseActivity
+import com.changs.movielist.data.model.FilmsModelItem
+import com.changs.movielist.databinding.ActivitySecondBinding
+
+class SecondActivity : BaseActivity<ActivitySecondBinding>(ActivitySecondBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val data = intent.getSerializableExtra("SecondActivity") as? FilmsModelItem
+
+        if (data != null) {
+            Glide.with(this)
+                .load(data.image)
+                .into(binding.secondImage)
+            binding.secondTitle.text = data.title
+            binding.secondScore.text = data.rt_score
+            binding.secondScenario.text = data.producer
+            binding.secondReleaseDate.text = data.release_date
+            binding.secondRunningTime.text = data.running_time
+            binding.secondDescription.text = data.description
+            binding.secondDirectorName.text = data.director
+        }
+    }
+}
