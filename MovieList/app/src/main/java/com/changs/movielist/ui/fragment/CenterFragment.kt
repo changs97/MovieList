c8dc08258c3494382a976fec93d2a3a412c0306d
Changhyung <pck4949@naver.com>
2022-02-12 15:30:37 +0900

    Create CenterFragment.kt

---
+++ MovieList/app/src/main/java/com/changs/movielist/ui/fragment/CenterFragment.kt
@@ -0,0 +1,27 @@
+package com.changs.movielist.ui.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.changs.movielist.R
+import com.changs.movielist.config.BaseFragment
+import com.changs.movielist.databinding.FragmentCenterBinding
+import com.changs.movielist.ui.activity.SplashActivity
+import com.changs.movielist.ui.adapter.RecyclerViewAdapter
+
+
+class CenterFragment :  BaseFragment<FragmentCenterBinding>(FragmentCenterBinding::bind, R.layout.fragment_center) {
+    private var adapter : RecyclerViewAdapter? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        centerRecyclerSetUp()
+    }
+    private fun centerRecyclerSetUp() {
+        val titleSortedFilmsList = SplashActivity.filmsList.sortedByDescending{ it.title }.reversed()
+        val titleSortedFilmsArrayList = ArrayList(titleSortedFilmsList)
+        adapter = RecyclerViewAdapter(titleSortedFilmsArrayList)
+        binding.centerFragmentRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.centerFragmentRecyclerView.adapter = adapter
+    }
+}
